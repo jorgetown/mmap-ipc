2b1bad705a3950bd863d670e70e8dd6b5c079c83
jdecastro <jorgetown@gmail.com>
2018-09-03 22:48:27 +0100

    Simplified Prime

--- src/main/java/jdc/Prime.java
+++ src/main/java/jdc/Prime.java
@@ -1,52 +1,13 @@
 package jdc;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Function;
 
 /**
  * @author Jorge De Castro
  */
 public final class Prime {
-    private final Function<Integer, Answer> primeFunction;
-    private final IpcQueueReader<Integer> intsQueue;
-    private final IpcQueueWriter<Answer> answersQueue;
-    private final ExecutorService executorService;
-
-    public Prime(@NotNull final Function<Integer, Answer> primeFunction,
-                 @NotNull final IpcQueueReader<Integer> intsQueue,
-                 @NotNull final IpcQueueWriter<Answer> answersQueue) {
-
-        this.primeFunction = primeFunction;
-        this.intsQueue = intsQueue;
-        this.answersQueue = answersQueue;
-        this.executorService = Executors.newCachedThreadPool();
-    }
-
-    public void start() {
-        executorService.execute(() -> {
-                    System.out.println("Prime ints consumer started...\n");
-
-                    while (!Thread.currentThread().isInterrupted() && !answersQueue.isFull()) {
-                        Integer i = intsQueue.poll();
-
-                        if (i == null) continue;
-
-                        Answer p = primeFunction.apply(i);
-                        answersQueue.add(p);
-                    }
-
-                    System.out.println("Prime task stopped after publishing 2GBs of answers...\n");
-                }
-        );
-    }
-
-    public void stop() {
-        executorService.shutdown();
-    }
 
     public static void main(String[] args) throws IOException {
         SingleQueueReader<Integer> intsQueue = SingleQueueReader.intsReader("PrimeIntsQueueReader");
@@ -55,14 +16,24 @@ public final class Prime {
 
         Primality primality = new Primality();
 
-        Prime prime = new Prime(
-                primality,
-                intsQueue,
-                answersQueue
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        executorService.execute(() -> {
+                    System.out.println("Prime ints consumer started...\n");
+
+                    while (!Thread.currentThread().isInterrupted() && !answersQueue.isFull()) {
+                        Integer i = intsQueue.poll();
+
+                        if (i == null) continue;
+
+                        Answer p = primality.apply(i);
+                        answersQueue.add(p);
+                    }
+
+                    System.out.println("Prime task stopped after publishing 2GBs of answers...\n");
+                }
         );
 
-        prime.start();
-
-        prime.stop();
+        executorService.shutdown();
     }
 }
